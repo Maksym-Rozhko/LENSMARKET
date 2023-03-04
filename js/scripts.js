0ff8303dad1e09a4e35f495983cea5ed6e10686d
Maksym <maksymRozhko7@gmail.com>
2023-03-04 21:46:35 +0200

    main-screen, offers, fix font

--- js/scripts.js
+++ js/scripts.js
@@ -12,14 +12,20 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
       
-    // new Swiper('.swiper', {
-    //     slidesPerView: 2,
-    //     loop: true,
-      
-    //     // If we need pagination
-    //     pagination: {
-    //       el: '.swiper-pagination',
-    //       clickable: true,
-    //     },
-    // });
+    new Swiper('.main-screen-slider.swiper', {
+        slidesPerView: 1,
+        loop: true,
+		speed: 1000,
+		spaceBetween: 25,
+        // Navigation arrows
+		navigation: {
+			nextEl: '.main-screen-slider .swiper-button-next',
+			prevEl: '.main-screen-slider .swiper-button-prev',
+		},
+        // If we need pagination
+        pagination: {
+          el: '.main-screen-slider .swiper-pagination',
+          clickable: true,
+        },
+    });
 });
